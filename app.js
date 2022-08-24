05a5549c712794b54d3ccf84e71dc376430063f9
Hosamov <hosamov@hotmail.com>
2022-08-24 14:42:37 -0700

    update dependencies, add userSchema

--- app.js
+++ app.js
@@ -1,16 +1,47 @@
+
+require('dotenv').config()
+
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const { Schema } = require('mongoose');
+const session = require('express-session');
 
+const app = express();
 // Initialize DB:
-require('./initDB')();
+require('./initDB')(); 
 
+app.set('view engine', 'pug');
+
+app.use('/static', express.static('public'));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(express.static("public"));
+
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+
+const UserSchema = new Schema( {
+  username: { type: String, default: 'Unknown' },
+  password: String,
+  email: String,
+  isAdmin: Boolean,
+  listItems: Array
+});
+
+
+//*  GET routes */
 app.get("/", (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(3000, () => {
+
+
+app.listen(process.env.PORT || 3000, () => {
   console.log('Server is running on port 3000...');
 });
