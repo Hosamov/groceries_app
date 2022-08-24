817da82d9ecf31eb8481b13b0584e9beed3efbb2
Hosamov <hosamov@hotmail.com>
2022-08-24 14:02:04 -0700

    update dependencies

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-const { application } = require('express');
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const { Schema } = require('mongoose');
+
+// Initialize DB:
+require('./initDB')();
 
 app.get("/", (req, res) => {
   res.send('Hello World!');
